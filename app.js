5f21c43a52f10015cad6424c024f331e073a72f6
ThomasRog3rs <Mr.Thomas.Rogers@proton.me>
2025-02-02 12:19:35 +0000

    try to fix for digital ocean deploy

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(function(err, req, res, next) {
   res.json({ error: err.message });
 });
 
-const port = process.env.PORT ?? 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
